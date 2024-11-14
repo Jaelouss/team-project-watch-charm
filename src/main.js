80b55f5aa5709445a11adb72ca30c7945384026b
Eugen Alekseev <devilsbrother1@gmail.com>
2024-11-14 15:17:27 +0200

    delete script

--- src/main.js
+++ src/main.js
@@ -15,15 +15,3 @@ toggleBtn.addEventListener("click", function() {
 hiddenItems.forEach(item => {
   item.style.display = "none";
 });
-
-
-//SALE секція thumbsnails
-const thumbnails = document.querySelectorAll(".thumbnail");
-const mainImage = document.getElementById("main-image");
-
-thumbnails.forEach(thumbnail => {
-  thumbnail.addEventListener("mouseover", function() {
-    const largeImageSrc = thumbnail.getAttribute("data-large");
-    mainImage.src = largeImageSrc;
-  });
-});
